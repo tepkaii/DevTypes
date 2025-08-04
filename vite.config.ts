98a658fb9197568b8ae1d320b5bf31c1ca44d4a1
abdulbasi-code <abuld0001@gmail.com>
2025-08-04 03:09:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
